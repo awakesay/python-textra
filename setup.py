81222d23ca503f56b6a66e3983e67a1baf45aa67
awakesay <awakesay@live.jp>
2024-06-09 18:47:21 +0900

    change setup.py

--- setup.py
+++ setup.py
@@ -19,53 +19,53 @@ INSTALL_REQUIRES = [
     'requests=>2.32.0'
 ]
 
-# EXTRAS_REQUIRE = {
-#     'tutorial': [
-#         'mlxtend>=0.18.0',
-#         'xgboost>=1.4.2',
-#     ]
-# }
+EXTRAS_REQUIRE = {
+    'tutorial': [
+        'requests>=2.32.0'
+    ]
+}
 
 PACKAGES = [
     'textra'
 ]
 
-CLASSIFIERS: list[str] = [
-    # 'Intended Audience :: Science/Research',
-    # 'License :: OSI Approved :: BSD License',
-    # 'Programming Language :: Python :: 3',
-    # 'Programming Language :: Python :: 3.6',
-    # 'Programming Language :: Python :: 3.7',
-    # 'Programming Language :: Python :: 3.8',
-    # 'Programming Language :: Python :: 3.9',
-    # 'Programming Language :: Python :: 3 :: Only',
-    # 'Topic :: Scientific/Engineering',
-    # 'Topic :: Scientific/Engineering :: Visualization',
-    # 'Topic :: Scientific/Engineering :: Artificial Intelligence',
-    # 'Topic :: Multimedia :: Graphics',
-    # 'Framework :: Matplotlib',
+CLASSIFIERS = [
+    'Intended Audience :: Science/Research',
+    'License :: OSI Approved :: BSD License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+    'Programming Language :: Python :: 3 :: Only',
+    'Topic :: Scientific/Engineering',
+    'Topic :: Scientific/Engineering :: Visualization',
+    'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    'Topic :: Multimedia :: Graphics',
+    'Framework :: Matplotlib',
 ]
 
-# with open('README.rst', 'r') as fp:
-#     readme = fp.read()
+with open('README.md', 'r') as fp:
+    readme = fp.read()
 # with open('CONTACT.txt', 'r') as fp:
 #     contacts = fp.read()
-# long_description = readme + '\n\n' + contacts
+long_description = readme  # + '\n\n' + contacts
 
-setup(name=NAME,
-      author=AUTHOR,
-      author_email=AUTHOR_EMAIL,
-      maintainer=AUTHOR,
-      maintainer_email=AUTHOR_EMAIL,
-      description=DESCRIPTION,
-    #   long_description=long_description,
-      license=LICENSE,
-      url=URL,
-      version=VERSION,
-      download_url=DOWNLOAD_URL,
-      python_requires=PYTHON_REQUIRES,
-      install_requires=INSTALL_REQUIRES,
-    #   extras_require=EXTRAS_REQUIRE,
-      packages=PACKAGES,
-      classifiers=CLASSIFIERS
+setup(
+    name=NAME,
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    maintainer=AUTHOR,
+    maintainer_email=AUTHOR_EMAIL,
+    description=DESCRIPTION,
+    long_description=long_description,
+    license=LICENSE,
+    url=URL,
+    version=VERSION,
+    download_url=DOWNLOAD_URL,
+    python_requires=PYTHON_REQUIRES,
+    install_requires=INSTALL_REQUIRES,
+    extras_require=EXTRAS_REQUIRE,
+    packages=PACKAGES,
+    classifiers=CLASSIFIERS
 )
